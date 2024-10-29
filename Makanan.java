add30415e1aa5d924c3bb25f5f261f8393a61593
JulianDava77 <2300018336@webmail.uad.ac.id>
2024-10-29 21:47:24 +0700

    Add files via upload

---
+++ Makanan.java
@@ -0,0 +1,5 @@
+public class Makanan extends Menu { // Inheritance
+    public Makanan(String nama, double harga) {
+        super(nama, harga);
+    }
+}
---
+++ Minuman.java
@@ -0,0 +1,5 @@
+public class Minuman extends Menu {  // Inheritance
+    public Minuman(String nama, double harga) {
+        super(nama, harga);
+    }
+}
